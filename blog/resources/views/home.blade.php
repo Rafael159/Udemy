ea00c6be9703e32b8d8a14cbf4c748562886733b
Rafael Alves <rafael-hyuga@hotmail.com>
2019-07-03 10:21:24 -0300

    Sincronizando projetos

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -7,6 +7,23 @@
         <div class="col-md-10 col-md-offset-1">
             <painel titulo="Dashboard">
                 Teste de conteúdo
+                <div class="row">
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 1" cor="blue">
+                            Teste 1
+                        </painel>
+                    </div>
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 2" cor="panel-warning">
+                            Teste 2
+                        </painel>
+                    </div>
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 3" cor="orange">
+                            Teste 3
+                        </painel>
+                    </div>
+                </div>
             </painel>
         </div>
     </div>
